9c55e26bae69f81c70ae6773acf9fe7200790d75
alvaroQ <alvaroqp@gmail.com>
2016-09-01 18:51:53 +0200

    proyecto ConversorDeDivisas

---
+++ ConversorDeDivisas/ConversorDeDivisasPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+
+namespace ConversorDeDivisas
+{
+	public partial class ConversorDeDivisasPage : ContentPage
+	{
+		public ConversorDeDivisasPage()
+		{
+			InitializeComponent();
+		}
+
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			if (string.IsNullOrEmpty(eurosEntry.Text))
+			{
+				labelInfo.Text = "Introduzca una cantidad en euros";
+				eurosEntry.Focus();
+				return;
+			}
+
+			textLibras.IsVisible = true;
+			textDolares.IsVisible = true;
+
+			decimal euros = decimal.Parse(eurosEntry.Text);
+			textLibras.Text = string.Format("{0:N2}", Conversion.ToLibras(euros)) + " libras";
+			textDolares.Text = string.Format("{0:N2}", Conversion.ToDolares(euros)) + " dolares";
+
+		}
+	}
+}
+
---
+++ ConversorDeDivisas/OperacionesConDivisas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ConversorDeDivisas
+{
+	class Conversion
+	{
+		public static decimal ToDolares(decimal euros)
+		{
+			decimal dolares = euros * 1.3M;
+			return dolares;
+		}
+		public static decimal ToLibras(decimal euros)
+		{
+			decimal libras = euros * 1.42M;
+			return libras;
+		}
+	}
+}
+
